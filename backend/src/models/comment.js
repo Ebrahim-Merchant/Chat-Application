953f5687fb6965ab0f033241ab86edf0f591f9ea
Merchant, Ebrahim <Ebrahim.Merchant@bmo.com>
2021-01-05 03:12:44 -0500

    Merge branch 'master' into develop

--- backend/src/models/comment.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
-
-const CommentSchema = new Schema({
-    author: String,
-    text: String,
-}, {timestamps: true});
-
-export default mongoose.model('Comment', CommentSchema);
